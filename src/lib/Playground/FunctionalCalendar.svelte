cb8832ce3d02b2906f3744c73e4cde76db3508c6
Elton Lin <eltonlin1998@gmail.com>
2024-11-01 16:42:24 +0900

    Messy WIP

--- src/lib/Playground/FunctionalCalendar.svelte
+++ src/lib/Playground/FunctionalCalendar.svelte
@@ -6,7 +6,7 @@
   import Tasks from "/src/back-end/Tasks"
   import { buildCalendarDataStructures } from '/src/helpers/maintainState.js'
 
-  import { onMount } from 'svelte'
+  import { onMount, tick } from 'svelte'
   import { DateTime } from 'luxon'
   import { tasksScheduledOn, user, calendarTasks, hasInitialScrolled } from '/src/store.js'
 
@@ -20,6 +20,59 @@
 
   let calOriginDT = DateTime.now().startOf('day').minus({ days: TOTAL_COLUMNS / 2 })
   let dtOfActiveColumns = []
+
+  let isShowingDockingArea = true
+  let initialHeaderHeight
+  let minRequiredHeight 
+  let H
+  let clientHeight 
+  // height vs clientHeight
+
+  let prevClientHeight
+
+  $: {
+    console.log('clientHeight =', clientHeight)
+    if (clientHeight >= CORNER_LABEL_HEIGHT) {
+      styleHeight = clientHeight + 1
+      console.log('manual height =', styleHegiht)
+
+      const roundedHeight = Math.round(clientHeight)
+      if (roundedHeight !== prevClientHeight) { // prevent infinite loop due to 
+        styleHeight = roundedHeight + 1
+        console.log('updated to clientHeight =', clientHeight)
+      }
+      // do a max height and min-height logic
+    }
+  }
+
+  let previousH
+  let initialHydratedHeight
+  let isInitial = true
+
+  // $: if (initialHydratedHeight) {
+  //   if (!previousH) {
+  //     H = initialHydratedHeight
+  //     previousH = H
+  //     console.log('H =', H)
+  //   }
+  // }
+
+
+  $: if ($tasksScheduledOn && isInitial) {
+    isInitial = false
+    H = computeDockingAreaHeight({ 
+      leftBoundIdx: leftEdgeIdx - 2*c, 
+      rightBoundIdx: rightEdgeIdx + 2*c 
+    })
+  }
+
+  function toggleDockingArea () {
+    isShowingDockingArea = !isShowingDockingArea
+    if (!isShowingDockingArea) {
+      minRequiredHeight = CORNER_LABEL_HEIGHT
+      initialHeaderHeight = 0
+    }
+  }
 
   let leftTriggerIdx 
   let rightTriggerIdx
@@ -50,7 +103,7 @@
     updateActiveColumns()
   })
 
-  function updateActiveColumns () {
+  async function updateActiveColumns () {
     const output = []
     for (let i = leftEdgeIdx - 2*c; i <= rightEdgeIdx + 2*c; i++) {
       output.push(
@@ -63,20 +116,70 @@
     prevLeftEdgeIdx = leftEdgeIdx
   }
 
+  function resizeDockingArea ({ leftBoundIdx, rightBoundIdx}) {
+    requestAnimationFrame(() => {
+      H = computeDockingAreaHeight({ leftBoundIdx, rightBoundIdx })
+      previousH = H
+      console.log('H =', H)
+    })
+  }
+
   function reactToScroll (leftEdgeIdx, rightEdgeIdx) {
     // note: `leftEdgeIdx` jumps non-consecutively sometimes depending on how fast the user is scrolling
     if (leftEdgeIdx <= leftTriggerIdx && leftEdgeIdx !== prevLeftEdgeIdx) {
-      fetchMorePastTasks(leftTriggerIdx) // even though jumps can be arbitrarily wide, the function calls will resolve in a weakly decreasing order of their `leftTriggerIdx`
+      fetchMorePastTasks(leftTriggerIdx).then(() => {
+        resizeDockingArea({ 
+          leftBoundIdx: leftTriggerIdx - 3*c, 
+          rightBoundIdx: rightEdgeIdx + c // `rightEdgeIdx` is scoped within this function
+        })
+      }) // even though jumps can be arbitrarily wide, the function calls will resolve in a weakly decreasing order of their `leftTriggerIdx`
       leftTriggerIdx -= (2*c + 1)
     } 
     else if (rightEdgeIdx >= rightTriggerIdx && rightEdgeIdx !== prevRightEdgeIdx) {
-      fetchMoreFutureTasks(rightTriggerIdx)
+      fetchMoreFutureTasks(rightTriggerIdx).then(() => {
+        resizeDockingArea({ 
+          leftBoundIdx: leftEdgeIdx - c, 
+          rightBoundIdx: rightTriggerIdx + 3*c // `rightTriggerIdx` is scoped within this function
+        })
+      })
       rightTriggerIdx += (2*c + 1)
     }
     
     if (leftEdgeIdx <= prevLeftEdgeIdx - c || rightEdgeIdx >= prevRightEdgeIdx + c) {
       updateActiveColumns()
     }
+  }
+
+  function computeDockingAreaHeight ({ leftBoundIdx, rightBoundIdx }) {
+    let maxHeightInBatch = CORNER_LABEL_HEIGHT
+    for (let i = leftBoundIdx; i <= rightBoundIdx; i++) {
+      const currentDT = calOriginDT.plus({ days: i })
+      const yyyyMMdd = currentDT.toFormat('yyyy-MM-dd')
+
+      let totalHeaderHeight = CORNER_LABEL_HEIGHT
+      if ($tasksScheduledOn[yyyyMMdd]) {
+        const { noStartTime } = $tasksScheduledOn[yyyyMMdd]
+        const { hasIcon, noIcon } = noStartTime
+
+        const baseHeaderHeight = 55
+
+        const noIconTopMargin = 8
+        const iconTasksHeight = Math.ceil(hasIcon.length / 6) * 37
+        const normalTasksHeight = noIcon.length * (14 + noIconTopMargin) 
+
+        const paddingAboveAllTasks = 6
+        const topPadding = 36 // main-content-top-margin
+        const botPadding = 18
+
+        totalHeaderHeight = topPadding + botPadding + paddingAboveAllTasks + baseHeaderHeight + iconTasksHeight + normalTasksHeight
+      }
+
+      if (totalHeaderHeight > maxHeightInBatch) {
+        maxHeightInBatch = totalHeaderHeight
+      }
+    }
+    console.log('maxHeightInBatch =', maxHeightInBatch)
+    return maxHeightInBatch
   }
 
   async function fetchMorePastTasks (triggerIdx) {
@@ -114,7 +217,8 @@
 </script>
 
 <div class="calendar-wrapper" style="position: relative;">
-  <div class="corner-label" style="height: {CORNER_LABEL_HEIGHT}px;">
+  <!-- TODO: make this a reusable component that displays the real YYYY -->
+  <div class="corner-label" style="height: {H}px;">
     <div style="font-size: 16px; margin-top: var(--main-content-top-margin);">
       <div style="color: rgb(0, 0, 0); font-weight: 400;">
         {monthName}
@@ -123,6 +227,15 @@
         2024
       </div>
     </div>
+
+    {#if $tasksScheduledOn}
+      <span
+        on:click={toggleDockingArea}
+        class="collapse-arrow material-symbols-outlined"
+      >
+        {isShowingDockingArea ? "expand_less" : "expand_more"}
+      </span>
+    {/if}
   </div>
 
   <div bind:this={ScrollParent}
@@ -136,20 +249,25 @@
       style="display: flex; background-color: var(--calendar-bg-color);"
     >
       {#if dtOfActiveColumns.length > 0 && $tasksScheduledOn}
-        <FunctionalCalendarTimestamps topMargin={CORNER_LABEL_HEIGHT}/>
+        <FunctionalCalendarTimestamps 
+          topMargin={H}
+        />
 
         <div 
           class="visible-days"
           style:transform={`translateX(${dtOfActiveColumns[0]?.diff(calOriginDT, 'days').days * COLUMN_WIDTH}px)`}
-        >
-          <div class="headers" class:bottom-border={$tasksScheduledOn}>
+        >          
+          <div 
+            class="headers-flexbox" 
+            class:bottom-border={$tasksScheduledOn}
+          >
             {#each dtOfActiveColumns as currentDate, i (currentDate.toMillis() + `${i}`)}
               <ReusableCalendarHeader
                 ISODate={currentDate.toFormat('yyyy-MM-dd')}
-                isShowingDockingArea={false}
+                {isShowingDockingArea}
                 on:task-update
                 on:task-click
-                on:task-checkbox-change
+                                                   
                 on:new-root-task
               />
             {/each}
@@ -210,12 +328,15 @@
     left: 60px; /* Timestamp width */
   }
 
-  .headers {
-    display: flex;
+  .headers-flexbox {
     position: sticky;
     top: 0;
     background: var(--calendar-bg-color);
     z-index: 1;
+
+    display: flex;
+
+    transition: all 500ms ease-out;
   }
 
   .day-columns {
@@ -224,5 +345,15 @@
 
   .bottom-border {
     border-bottom: 1px solid lightgrey;
+  }
+
+  .collapse-arrow {
+    position: absolute;
+    bottom: 4px;
+    right: 4px;
+    font-size: 26px;
+    cursor: pointer;
+    color: rgb(121, 121, 121);
+    font-weight: 300;
   }
 </style> 
